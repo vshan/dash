f21970e10674bdebc92a0d025e86c546eaf755ee
Vinay Bhat <vinaybhat001@gmail.com>
2016-01-18 18:34:56 +1000

    Minor changes

--- lib/util.c
+++ lib/util.c
@@ -46,32 +46,45 @@ char** str_split(char* a_str, const char a_delim)
     return result;
 }
 
+int dash_eval(char *line, char *std_input)
+{
+  if (line == NULL)
+    return 1;
+
+  if (std_input != NULL)
+  {
+    
+  }
+
+  char **tokens = str_split(line, ' ');
+  char *token;
+  int i;
+  int num_tokens = no_of_sub_strings(tokens);
+
+  for (i = 0; i < num_tokens; i++) {
+      if (strcmp(tokens[i], REMOTE_PIPE) == 0) {
+          if (i == 0) {
+              fprintf(stderr, "Please ensure there is expression behind remote pipe.\n", );
+              return 0;
+          }
+          else if (i == num_tokens) {
+              fprintf(stderr, "Please ensure there is expression after remote pipe.\n", );
+              return 0;
+          }
+          char *subcommand = join_strings(tokens, ' ', i+1, num_tokens);
+          char *remotehost = extract_host(subcommand);
+          char *maincommand = join_strings(tokens, ' ', 0, i);
+          char *msg_data = dash_exec_scmd(maincommand_tokens, num_mc_tokens);
+          char *pip_msg = dashp_pip(msg_data, subcommand, remotehost);
+          send_to_host(pip_msg, remotehost);
+          
+      }
+  }
+}
+
 char** tokenize(char *line)
 {
-    char **tokens = str_split(line, ' ');
-    char *token;
-    int i;
-    int num_tokens = no_of_sub_strings(tokens);
-
-    for (i = 0; i < num_tokens; i++) {
-        if (strcmp(tokens[i], REMOTE_PIPE) == 0) {
-            if (i == 0) {
-                fprintf(stderr, "Please ensure there is expression behind remote pipe.\n", );
-                return 0;
-            }
-            else if (i == num_tokens) {
-                fprintf(stderr, "Please ensure there is expression after remote pipe.\n", );
-                return 0;
-            }
-            char *subcommand = join_strings(tokens, ' ', i+1, num_tokens);
-            char *remotehost = extract_host(subcommand);
-            char *maincommand = join_strings(tokens, ' ', 0, i);
-            char *msg_data = dash_exec_scmd(maincommand_tokens, num_mc_tokens);
-            char *pip_msg = dashp_pip(msg_data, subcommand, remotehost);
-            send_to_host(pip_msg, remotehost);
-            
-        }
-    }
+    
 }
 
 char* dash_exec_scmd(char **tokens, int num)
--- src/main.c
+++ src/main.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]) {
       }
       else if (dmsg->dsp == DASHP_PIP) {
         char *command = dmsg->command;
-        dash_eval(command, 0);
+        dash_eval(command, dmg->payload);
       }
     }
 
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
     while (1) {
       printf("%s ", promt);
       char *line = dash_read_line();
-      rv = dash_eval(line, DASH_EXEC);
+      rv = dash_eval(line, NULL);
       if (rv == INVOLVED_NETWORKING)
         read(fd[1]);
     }
